22119fd1bc053d651fc30cd27b1e74534f4cbe32
Vadym <vad.evlanov@gmail.com>
2020-12-23 22:29:20 +0200

    add tmdb api

--- src/services/tmdb-api.js
+++ src/services/tmdb-api.js
@@ -1,19 +1,41 @@
 import axios from 'axios';
 
-const BASE_URL = '';
-const API_KEY = '';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '8978731d3453660c119868bf0fe3e32f';
 
 axios.defaults.baseURL = BASE_URL;
-axios.defaults.params = { key: API_KEY };
+axios.defaults.params = { api_key: API_KEY, language: 'en-EN' };
 
-async function getTrandingMovie() {
-  try {
-    const data = await axios.get('');
-    console.log(data);
-  } catch (error) {
-    console.log('error', error);
-    return [];
-  }
+export async function fetchTrandingMovie(page = 1) {
+  const {
+    data: { results },
+  } = await axios.get(`/trending/movie/day?page=${page}`);
+  return results;
 }
 
-export default { getTrandingMovie };
+export async function fetchMovie(query, page = 1) {
+  const {
+    data: { results },
+  } = await axios.get(`/search/movie?query=${query}`);
+  return results;
+}
+
+export async function fetchMovieDetails(movieId) {
+  const { data } = await axios.get(`/movie/${movieId}`);
+  return data;
+}
+
+export async function fetchMovieCredits(movieId) {
+  const {
+    data: { cast },
+  } = await axios.get(`/movie/${movieId}/credits`);
+  return cast;
+}
+
+export async function fetchMovieReviews(movieId) {
+  const {
+    data: { results },
+  } = await axios.get(`/movie/${movieId}/reviews`);
+
+  return results;
+}
